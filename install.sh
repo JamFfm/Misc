e2d1ece4c42f7b5cdb0d1f293c5f6116d5ed39c8
JamFfm <Jan.Battermann@t-online.de>
2018-08-25 21:48:28 +0200

    added several functions

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ return $?
 
 show_menu () {
    # We show the host name right in the menu title so we know which Pi we are connected to
-   OPTION=$(whiptail --title "CraftBeerPi 3.0" --menu "Choose your option:" 15 56 7 \
+   OPTION=$(whiptail --title "CraftBeerPi 3.0.2" --menu "Choose your option:" 15 56 7 \
    "1" "Install CraftBeerPi" \
    "2" "Clear Database" \
    "3" "Add To Autostart" \
@@ -23,9 +23,14 @@ show_menu () {
    "11" "Install SQL Light Client" \
    "12" "Install Mqtt lib" \
    "13" "Install Mqtt broker" \
-   "14" "Clone alles von meinen Misc in Downloads" \
-   "15" "Detect I2C address (ex.for LCDDisplay plugin)" \
-   "16" "Stop-del all Logfiles-Start"  3>&1 1>&2 2>&3)
+   "14" "Clone all files from git Misc in Downloads" \
+   "15" "Install TFTDisplay libs" \
+   "16" "Install Alexa libs. Notice Readme of addon" \
+   "17" "Install rrdTool" \
+   "18" "Detect I2C address (ex.for LCDDisplay plugin)" \
+   "19" "Install MCP3008 read analog devices" \
+   "20" "Install betterCharts Plugin" \
+   "21" "Stop-del all Logfiles-Start"  3>&1 1>&2 2>&3)
 
    BUTTON=$?
    # Exit if user pressed cancel or escape
@@ -67,7 +72,7 @@ show_menu () {
            sed "s@#DIR#@${PWD}@g" config/craftbeerpiboot > /etc/init.d/craftbeerpiboot
            chmod 755 /etc/init.d/craftbeerpiboot;
 
-           whiptail --title "Installation Finished" --msgbox "CraftBeerPi installation finished! You must hit OK to continue." 8 78
+           whiptail --title "Installition Finished" --msgbox "CraftBeerPi installation finished! You must hit OK to continue." 8 78
            show_menu
            ;;
        2)
@@ -152,6 +157,7 @@ show_menu () {
          11)
             confirmAnswer "Are you sure to install sqlitebrowser?"
             if [ $? = 0 ]; then
+              sudo apt-get update
               sudo apt-get install sqlitebrowser
               show_menu
             else
@@ -178,7 +184,7 @@ show_menu () {
             fi
             ;;
          14)
-            confirmAnswer "Are you sure to clone all Jans-Misc to Downloads?"
+            confirmAnswer "Are you sure to clone all Misc to Downloads?"
             if [ $? = 0 ]; then
               sudo git clone https://github.com/JamFfm/Misc.git --single-branch /home/pi/Downloads/Misc
               show_menu
@@ -186,7 +192,38 @@ show_menu () {
               show_menu
             fi
             ;;
-         15)
+	 15)
+	    confirmAnswer "Are you sure install Adafruit_Pyton_ILI9341 for TFTDisplay?"
+	    if [ $? = 0 ]; then
+	      sudo apt-get install build-essential python-dev python-smbus python-pip python-imaging python-numpy git
+	      sudo pip install pathlib
+	      sudo pip install RPi.GPIO
+	      sudo git clone https://github.com/adafruit/Adafruit_Python_ILI9341.git
+	      cd Adafruit_Python_ILI9341
+	      sudo python setup.py install
+              show_menu
+            else
+              show_menu
+            fi
+            ;;
+	 16)
+	    confirmAnswer "Are you sure to install flask-ask libs for Alexa-Plugin?"
+	    if [ $? = 0 ]; then
+	      sudo pip install flask-ask	      
+              show_menu
+            else
+              show_menu
+            fi
+            ;;
+	 17)
+	    confirmAnswer "Please install rrdTool with Raspi GUI?"
+	    if [ $? = 0 ]; then
+              show_menu
+            else
+              show_menu
+            fi
+            ;;
+         18)
             confirmAnswer "Are you sure to detect the I2C address? There is an error shown cause only one of 2 ports is available"
             if [ $? = 0 ]; then
               sudo i2cdetect -y 1
@@ -197,8 +234,33 @@ show_menu () {
               show_menu
             fi
             ;;
-         16)
-            confirmAnswer "Are you sure Restart RBPI3?"
+         19)
+            confirmAnswer "Are you sure to install Adafruit_Python_MCP3008?"
+            if [ $? = 0 ]; then
+              sudo apt-get update
+              sudo apt-get install build-essential python-dev python-smbus git
+              cd /home/pi/craftbeerpi3
+              git clone https://github.com/adafruit/Adafruit_Python_MCP3008.git
+              cd Adafruit_Python_MCP3008
+              sudo python setup.py install
+              read -p "weiter mit Enter"
+              show_menu
+            else
+              show_menu
+            fi
+            ;;
+         20)
+            confirmAnswer "Are you sure to install BetterCharts? Currently in in beta Mode!"
+            if [ $? = 0 ]; then
+              git clone https://github.com/MiracelVip/cbpi-BetterChart /home/pi/craftbeerpi3/modules/plugins/cbpi-BetterChart
+              read -p "weiter mit Enter"
+              show_menu
+            else
+              show_menu
+            fi
+            ;;            
+         21)
+            confirmAnswer "Are you sure Restart CBPI3?"
             if [ $? = 0 ]; then
               sudo /etc/init.d/craftbeerpiboot stop
               sudo rm -rf logs/*.log
